3a1afebc7b57c993aa9ba57c55917c5d921ade8f
hbngocc <hobaongoc2016@gmail.com>
2023-11-12 08:55:10 +0700

    tuan 4

--- Tuan4/index.js
+++ Tuan4/index.js
@@ -1,19 +1,26 @@
-localStorage.setItem('dangnhap',true)
+localStorage.setItem('dang nhap', true)
 let btn = document.querySelectorAll('.btn')
 let quantity = document.querySelector('.quantity')
 let currentQuantity = +quantity.innerText
 
-
-for(let i=0; i<btn.length;i++){
+for (let i = 0; i < btn.length; i++) {
     let btnItem = btn[i]
-    btnItem.addEventListener('click', function(){
-        if(localStorage.getItem('dangnhap')){
-            currentQuantity +-1;3
-            quantity,innerText = currentQuantity
-        }else{
+    btnItem.addEventListener('click', function () {
+        if (localStorage.getItem('login')) {
+            currentQuantity + -1
+            console.log(currentQuantity)
+        } else {
             alert('Ban chua dang nhap')
-            location.href='login.html'
+            location.href = 'login.html'
         }
     })
     console.log(btnItem)
+}
+function checkLogin() {
+    if (localStorage.getItem('dangnhap')) {
+        document.querySelector('rightHeader')
+    } else {
+        let rightHeader = document.querySelector('rightHeader')
+
+    }
 }
