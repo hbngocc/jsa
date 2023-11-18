6e2ccbf0993e9b3e68578465ee984242536a0862
hbngocc <hobaongoc2016@gmail.com>
2023-11-18 15:40:41 +0700

    tuan 4

--- Tuan4/dangnhap.js
+++ Tuan4/dangnhap.js
@@ -8,30 +8,39 @@ let errorMessagePassword = errorMessages[1];
 // Bien chua tai khoan he thong
 
 const account = {
-  email: "khang123@gmail.com",
-  pass: "123123",
+    email: "khang123@gmail.com",
+    pass: "123123",
 };
 
 // Đặt sự kiện click vào nút login
 btnLogin.addEventListener("click", function () {
-  let emailValue = emailInput.value;
-  let passValue = passwordInput.value;
+    let emailValue = emailInput.value;
+    let passValue = passwordInput.value;
 
-  //   KIEM TRA TAI KHOAN
-  if (emailValue == account.email) {
-    // Dung tai khoan
-    if (passValue == account.pass) {
-      // Dung mk va tk
-      alert("Đăng nhập thành công");
-      location.href = "index.html";
-      localStorage.setItem("dangnhap", account.email);
+    //   KIEM TRA TAI KHOAN
+    if (emailValue == account.email) {
+        // Dung tai khoan
+        if (passValue == account.pass) {
+            // Dung mk va tk
+            alert("Đăng nhập thành công");
+            location.href = "index.html";
+            localStorage.setItem("dangnhap", account.email);
+        }
     } else {
-      // Sai mat khau
-      alert("Sai mat khau");
+        // SAI TAI KHOAN
+        emailInput.classList.add("error");
+        errorMessageEmail.innerText = "Sai tai khoan";
     }
-  } else {
-    // SAI TAI KHOAN
-    emailInput.classList.add("error");
-    errorMessageEmail.innerText = "Tai dep trai da sai tai khoan";
-  }
+
+    //Kiem tra email
+    if (emailValue == '') {
+        errorMessageEmail.innerText = "Email khong duoc de trong"
+    }
+
+    // kiem tra mat khau
+    if (passValue == '') {
+        errorMessagePassword.innerText = 'Vui long nhap mat khau'
+    } 
+    
+
 });
